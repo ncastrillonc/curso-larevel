36f2b84df26f848f8f579c844c0c3424118e36ca
USER <USER@idea-PC>
2015-03-14 17:50:27 -0500

    Clase 2 - Actualizo pagina

--- app/views/index.php
+++ app/views/index.php
@@ -134,93 +134,68 @@
             </div>
             <div class="row">
                 <div class="col-sm-3">
-                    B
+                    <!-- B -->
+                    <div class="jumbotron">
+                        <h1>Hello, world!</h1>
+                        <p>...</p>
+                        <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
+                      </div>
                 </div>
                 <div class="col-sm-9">
                     <div class="row">
                         <div class="col-sm-5 col-sm-offset-1">
                             <div class="row">
                                 <!-- C -->
-                                <form class="form-horizontal">
-                                    <div class="form-group">
-                                      <label for="inputEmail3" class="col-sm-2 control-label">Email</label>
-                                      <div class="col-sm-10">
-                                        <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
-                                      </div>
-                                    </div>
-                                    <div class="form-group">
-                                      <label for="inputPassword3" class="col-sm-2 control-label">Password</label>
-                                      <div class="col-sm-10">
-                                        <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
-                                      </div>
-                                    </div>
-                                    <div class="form-group">
-                                      <div class="col-sm-offset-2 col-sm-10">
-                                        <div class="checkbox">
-                                          <label>
-                                            <input type="checkbox"> Remember me
-                                          </label>
-                                        </div>
-                                      </div>
-                                    </div>
-                                    <div class="form-group">
-                                      <div class="col-sm-offset-2 col-sm-10">
-                                        <button type="submit" class="btn btn-default">Sign in</button>
-                                      </div>
-                                    </div>
-                                  </form>
+                                <ol class="breadcrumb">
+                                    <li><a href="#">Home</a></li>
+                                    <li><a href="#">Library</a></li>
+                                    <li class="active">Data</li>
+                                  </ol>
                             </div> 
                             <div class="row">
-                                D
+                                <!-- D -->
+                                <!-- 16:9 aspect ratio -->
+                                <div class="progress">
+                                  <div class="progress-bar progress-bar-success progress-bar-striped" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 40%">
+                                    <span class="sr-only">40% Complete (success)</span>
+                                  </div>
+                                </div>
+                                <div class="progress">
+                                  <div class="progress-bar progress-bar-info progress-bar-striped" role="progressbar" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100" style="width: 20%">
+                                    <span class="sr-only">20% Complete</span>
+                                  </div>
+                                </div>
+                                <div class="progress">
+                                  <div class="progress-bar progress-bar-warning progress-bar-striped" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%">
+                                    <span class="sr-only">60% Complete (warning)</span>
+                                  </div>
+                                </div>
+                                <div class="progress">
+                                  <div class="progress-bar progress-bar-danger progress-bar-striped" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width: 80%">
+                                    <span class="sr-only">80% Complete (danger)</span>
+                                  </div>
+                                </div>
                             </div> 
                         </div>
                         <div class="col-sm-4 col-sm-offset-1">
                             <div class="row">
                                 <!-- E -->
-                                <!-- Standard button -->
-                                <button type="button" class="btn btn-default">Default</button>
-
-                                <!-- Provides extra visual weight and identifies the primary action in a set of buttons -->
-                                <button type="button" class="btn btn-primary">Primary</button>
-
-                                <!-- Indicates a successful or positive action -->
-                                <button type="button" class="btn btn-success">Success</button>
-
-                                <!-- Contextual button for informational alert messages -->
-                                <button type="button" class="btn btn-info">Info</button>
-
-                                <!-- Indicates caution should be taken with this action -->
-                                <button type="button" class="btn btn-warning">Warning</button>
-
-                                <!-- Indicates a dangerous or potentially negative action -->
-                                <button type="button" class="btn btn-danger">Danger</button>
-
-                                <!-- Deemphasize a button by making it look like a link while maintaining button behavior -->
-                                <button type="button" class="btn btn-link">Link</button>
+                                <button type="button" class="btn btn-primary btn-lg btn-block">Block level button</button>
+                                <button type="button" class="btn btn-default btn-lg btn-block">Block level button</button>  
                             </div> 
                             <div class="row">
                                 <!-- F -->
-                                <form class="form-horizontal">
-                                    <div class="form-group has-success has-feedback">
-                                      <label class="control-label col-sm-3" for="inputSuccess3">Input with success</label>
-                                      <div class="col-sm-9">
-                                        <input type="text" class="form-control" id="inputSuccess3" aria-describedby="inputSuccess3Status">
-                                        <span class="glyphicon glyphicon-ok form-control-feedback" aria-hidden="true"></span>
-                                        <span id="inputSuccess3Status" class="sr-only">(success)</span>
-                                      </div>
-                                    </div>
-                                    <div class="form-group has-success has-feedback">
-                                      <label class="control-label col-sm-3" for="inputGroupSuccess2">Input group with success</label>
-                                      <div class="col-sm-9">
-                                        <div class="input-group">
-                                          <span class="input-group-addon">@</span>
-                                          <input type="text" class="form-control" id="inputGroupSuccess2" aria-describedby="inputGroupSuccess2Status">
-                                        </div>
-                                        <span class="glyphicon glyphicon-ok form-control-feedback" aria-hidden="true"></span>
-                                        <span id="inputGroupSuccess2Status" class="sr-only">(success)</span>
-                                      </div>
-                                    </div>
-                                  </form>
+                                <address>
+                                    <strong>Twitter, Inc.</strong><br>
+                                    795 Folsom Ave, Suite 600<br>
+                                    San Francisco, CA 94107<br>
+                                    <abbr title="Phone">P:</abbr> (123) 456-7890
+                                  </address>
+
+                                  <address>
+                                    <strong>Full Name</strong><br>
+                                    <a href="mailto:#">first.last@example.com</a>
+                                  </address>
                             </div> 
                         </div>
                     </div>
